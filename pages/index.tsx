982ccfd1be4ed18a3775707f209fe5f047d6adf7
Bunlong <bunlong.van@gmail.com>
2022-12-19 22:48:38 +0700

    Add next-prism

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,10 @@ import {
   Container,
   Heading,
 } from '@chakra-ui/react'
+import { usePrism } from 'next-prism'
 
 export default function Home() {
+  const { Code } = usePrism()
   return (
     <>
       <Box bg={'#00c3ff'} px={4} color="white">
@@ -31,6 +33,18 @@ export default function Home() {
               <Heading as='h3' size='lg'>
                 Installation
               </Heading>
+            </Box>
+            <Box>
+              next-share is available on npm. It can be installed with the following command:
+            <Code language='javascript'>
+      {`npm install next-share --save`}
+    </Code>
+            </Box>
+            <Box>
+            next-share is available on yarn as well. It can be installed with the following command:
+            <Code language='javascript'>
+      {`yarn add next-share --save`}
+    </Code>
             </Box>
           </Stack>
         </Stack>
