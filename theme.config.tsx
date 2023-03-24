78e4af5e6cb861649aa0c3a75ab4bed092019727
Bunlong <bunlong.van@gmail.com>
2023-03-24 14:39:43 +0700

    v0.0.2

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – next-share'
+      }
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="next-share" />
+      <meta property="og:description" content="A social share buttons plugin for Next.js, Create React App, Gatsby.js as well as React apps." />
+    </>
+  ),
   logo: <span>next-share</span>,
   project: {
     link: 'https://github.com/bunlong/next-share',
@@ -11,8 +27,13 @@ const config: DocsThemeConfig = {
   // },
   docsRepositoryBase: 'https://github.com/next-share/next-share-site',
   footer: {
-    text: 'next-share document',
-  },
+    text:
+      <center>
+        <span>
+          next-share by <a href="https://github.com/bunlong" target="_blank">Bunlong</a> <br/> © 2022-{new Date().getFullYear()}
+        </span>
+      </center>
+  }
 }
 
 export default config
